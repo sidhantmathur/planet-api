ad3626c94e731cb92c5350338c00a82e317ee242
sidhantmathur <sidhant185@gmail.com>
2020-12-18 15:50:06 -0600

    planet routes

--- app/routes/planet_routes.js
+++ app/routes/planet_routes.js
@@ -1,114 +1,114 @@
-// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
-// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
-
-// pull in Mongoose model for planets
 const Planet = require('../models/planet')
-
-// this is a collection of methods that help us detect situations when we need
-// to throw a custom error
 const customErrors = require('../../lib/custom_errors')
-
-// we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
-// we'll use this function to send 401 when a user tries to modify a resource
-// that's owned by someone else
 const requireOwnership = customErrors.requireOwnership
-
-// this is middleware that will remove blank fields from `req.body`, e.g.
-// { planet: { title: '', text: 'foo' } } -> { planet: { text: 'foo' } }
-const removeBlanks = require('../../lib/remove_blank_fields')
-// passing this as a second argument to `router.<verb>` will make it
-// so that a token MUST be passed for that route to be available
-// it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
-// instantiate a router (mini app that only handles routes)
 const router = express.Router()
+
+// SHOW
+router.get('/planets/:id', (req, res, next) => {
+  Planet.findById(req.params.id)
+    .then(handle404)
+    .then(planet => res.status(200).json({ planet: planet.toObject() }))
+    .catch(next)
+})
+
+// router.get('/planets-rand/', (req, res, next) => {
+//   Planet.findById()
+//     .then(handle404)
+//     .then(planet => res.status(200).json({ planet: planet.toObject() }))
+//     .catch(next)
+// })
+
+// router.get('/planets/rand', (req, res, next) => {
+//   Planet.aggregate.sample(3)()
+//     .then(handle404)
+//     .then(planet => res.status(200).json({ planet: planet.toObject() }))
+//     .catch(next)
+// })
+
+// router.get('/planets-count', (req, res, next) => {
+//   Planet.countDocuments(function (count) {
+//     console.log('there are %d planets', count)
+//   })
+// })
+
+router.get('/planets-rand', (req, res, next) => {
+  // const count = Planet.estimatedDocumentCount()
+  // var random = Math.floor(Math.random() * count)
+  var random = Math.floor(Math.random() * 4000)
+
+  Planet.findOne()
+    .skip(random)
+    .then(handle404)
+    .then(planet => res.status(200).json({ planet: planet.toObject() }))
+    .catch(next)
+})
 
 // INDEX
 // GET /planets
 router.get('/planets', (req, res, next) => {
   Planet.find()
     .then(planets => {
-      // `planets` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
       return planets.map(planet => planet.toObject())
     })
-    // respond with status 200 and JSON of the planets
     .then(planets => res.status(200).json({ planets: planets }))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
-// SHOW
-// GET /planets/5a7db6c74d55bc51bdf39793
-router.get('/planets/:id', (req, res, next) => {
-  // req.params.id will be set based on the `:id` in the route
-  Planet.findById(req.params.id)
+// Show Planets by Name
+router.get('/planets-name', requireToken, (req, res, next) => {
+  // console.log(req.user)
+  Planet.find({'name': req.body.name})
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "planet" JSON
-    .then(planet => res.status(200).json({ planet: planet.toObject() }))
-    // if an error occurs, pass it to the handler
-    .catch(next)
-})
-
-// CREATE
-// POST /planets
-router.post('/planets', requireToken, (req, res, next) => {
-  // set owner of new planet to be current user
-  req.body.planet.owner = req.user.id
-
-  Planet.create(req.body.planet)
-    // respond to succesful `create` with status 201 and JSON of new "planet"
-    .then(planet => {
-      res.status(201).json({ planet: planet.toObject() })
+    .then(planets => {
+      return planets.map(planet => {
+        requireOwnership(req, planet)
+        return planet.toObject()
+      })
     })
-    // if an error occurs, pass it off to our error handler
-    // the error handler needs the error message and the `res` object so that it
-    // can send an error message back to the client
+    .then(planets => {
+    // console.log(planets)
+      res.status(200).json({ planets: planets })
+    })
     .catch(next)
 })
 
-// UPDATE
-// PATCH /planets/5a7db6c74d55bc51bdf39793
-router.patch('/planets/:id', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
-  delete req.body.planet.owner
-
-  Planet.findById(req.params.id)
+// Show Planets by Mass
+router.get('/planets-mass', requireToken, (req, res, next) => {
+  // console.log(req.user)
+  Planet.find({'massE': req.body.mass})
     .then(handle404)
-    .then(planet => {
-      // pass the `req` object and the Mongoose record to `requireOwnership`
-      // it will throw an error if the current user isn't the owner
-      requireOwnership(req, planet)
-
-      // pass the result of Mongoose's `.update` to the next `.then`
-      return planet.updateOne(req.body.planet)
+    .then(planets => {
+      return planets.map(planet => {
+        requireOwnership(req, planet)
+        return planet.toObject()
+      })
     })
-    // if that succeeded, return 204 and no JSON
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
+    .then(planets => {
+    // console.log(planets)
+      res.status(200).json({ planets: planets })
+    })
     .catch(next)
 })
 
-// DESTROY
-// DELETE /planets/5a7db6c74d55bc51bdf39793
-router.delete('/planets/:id', requireToken, (req, res, next) => {
-  Planet.findById(req.params.id)
+router.get('/planets-disc', requireToken, (req, res, next) => {
+  // console.log(req.user)
+  Planet.find({'discovered': req.body.mass})
     .then(handle404)
-    .then(planet => {
-      // throw an error if current user doesn't own `planet`
-      requireOwnership(req, planet)
-      // delete the planet ONLY IF the above didn't throw
-      planet.deleteOne()
+    .then(planets => {
+      return planets.map(planet => {
+        requireOwnership(req, planet)
+        return planet.toObject()
+      })
     })
-    // send back 204 and no content if the deletion succeeded
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
+    .then(planets => {
+    // console.log(planets)
+      res.status(200).json({ planets: planets })
+    })
     .catch(next)
 })
 
