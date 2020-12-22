323c0403b7be9ac283e7a5ffb6c6009bc8521d35
sidhantmathur <sidhant185@gmail.com>
2020-12-22 10:48:33 -0600

    add new data, and depr old data

---
+++ config/Data/mongo_load.sh
@@ -0,0 +1,6 @@
+
+# if path isn't working, do path from the root
+# run this command in terminal, not the mongo shell
+# csv seems to work better
+
+# mongoimport --db=planet-api-development --collection=planets --type=csv --headerline --drop --file=/Users/sidhantmathur/sei/projects/planet-api/config/plDataHabit.csv
